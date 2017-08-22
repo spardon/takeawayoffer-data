9c92f479a70734468947d7e0f993535df20dfe58
shipeidong <shipeidong@fellowplus.com>
2017-08-22 10:05:46 +0800

    fix 增加部分关键字

--- data/models/eleme.py
+++ data/models/eleme.py
@@ -20,11 +20,11 @@ class ElemeRestaurant(MongoDoc):
         name=basestring,  # 商户名称
         categroy=list,  # 所属分类，可能有多个
         address=basestring,  # 商户地址
-        phone_num=list,  # 电话 可能有多个
+        phone_num=basestring,  # 电话 可能有多个
         open_time=list,  # 营业时间 多段营业的情况
         rating=float,  # 评分
         rating_count=int,  # 评价总数
-        oreder_num=int,  # 最近订单数量
+        order_num=int,  # 最近订单数量
         latitude=float,  # 纬度
         longitude=float,  # 经度
         activity=list,  # 活动
@@ -35,4 +35,39 @@ class ElemeRestaurant(MongoDoc):
         {'field': 'city'},
         {'field': 'name'},
         {'field': 'rating'},
+    ]
+
+
+class ElemeAreaInfo(MongoDoc):
+    '''
+        饿了么相关定位信息
+        id: "10897473655955349371",
+        name: "杭州绿城育华学校",
+        address: "浙江省杭州市西湖区紫金港路杭州绿城育华学校旁",
+        short_address: "紫金港路杭州绿城育华学校旁",
+        latitude: 30.28947,
+        longitude: 120.08537,
+        city: "杭州市",
+        request_id: "3545425242943446650",
+        city_id: 2,
+        geohash: "wtmkkd52zym",
+        count: 2151
+    '''
+    __collection__ = 'eleme_area_info'
+
+    structure = dict(
+        name=basestring,
+        address=basestring,
+        short_address=basestring,
+        latitude=float,
+        longitude=float,
+        city=basestring,
+        city_id=int,
+        geohash=basestring,
+        count=int  # 附近商户数
+    )
+
+    indexs = [
+        {'field': 'name'},
+        {'field': 'geohash'}
     ]
